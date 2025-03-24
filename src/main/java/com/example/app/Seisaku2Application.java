7d1d9d207d7d08fa2ed26fb77de680870578d302
taka <takayuki@examlpe.com>
2025-03-24 12:54:59 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/app/Seisaku2Application.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seisaku2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seisaku2Application.class, args);
+	}
+
+}
